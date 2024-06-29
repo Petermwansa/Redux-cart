c6d0577dc2c8b1239552418a8bb857d78177beaa
Petermwansa <mwansapeter800@gmail.com>
2024-06-29 22:03:45 +0300

    Add thunks to make the code more leaner and

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useEffect, Fragment } from 'react';
 import { useDispatch } from 'react-redux';
-import { uiActions } from './store/ui-slice';
 import Notification from './components/UI/NOtification';
+import { sendCartData } from './store/cart-slice';
+
 
 
 
@@ -13,55 +14,19 @@ let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
-  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const showCart = useSelector((state) => state.ui.cartVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!',
-        })
-      );
-      const response = await fetch(
-        'https://redux-cart-ac513-default-rtdb.firebaseio.com/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart),
-        }
-      );
 
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!',
-        })
-      );
-    };
-
-    // this blovks the cart from being send when the app just starts 
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!',
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
+    
   }, [cart, dispatch]);
 
   return (
